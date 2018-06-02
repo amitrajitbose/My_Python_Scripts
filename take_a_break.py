740a34e03f0ad78098740e974d2ed083d51581ad
amitrajitbose <amitrajitbose9@gmail.com>
2018-06-02 21:52:30 +0530

    Documented TakeABreak.py

--- take_a_break.py
+++ take_a_break.py
@@ -3,5 +3,8 @@ import time
 print("Current Time: "+time.ctime()) #shows the current time
 n=int(input("How many breaks do you need? "))
 for loop in range(n):
+	'''
+	just change this time as per your requirement, this is in seconds.
+	Currently set at five seconds, to make the gap of thirty minutes put 30*60'''
     time.sleep(5)
-    webbrowser.open_new("https://youtu.be/b_n3IJfW8Dg?t=6")
+    webbrowser.open_new("https://youtu.be/b_n3IJfW8Dg?t=6") #I kinda like this song, you can customize
